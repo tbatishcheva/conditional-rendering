f5e3975ef5a087271b8c77825a9b4f3a4680debc
LAPTOP-AD9KDN1M\Tatyana <tat.batishcheva@gmail.com>
2018-12-22 10:26:30 +0300

    Add Conditional Rendering and Handling Events

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      condition: true,
+    };
+  }
+
+  handleClick = () => {
+    const { condition } = this.state;
+
+    this.setState({
+      condition: !condition,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <b>{this.state.condition ? <img src={logo} className="App-logo" alt="logo" /> : ''}</b>
+          <input onClick={this.handleClick} type="button" name="print" value="Hide/Show" />
         </header>
       </div>
     );
